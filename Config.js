d69cf623dce4bdee04f2370579ca693d49094b63
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-07-02 12:41:45 +0100

    Delete Config.js

--- Config.js
+++
@@ -1,5 +0,0 @@
-
-
-module.exports.Salt = "3FJSei8zPx";
-module.exports.DB_URL = "mongodb://63.35.236.7:27017";
-module.exports.TeamDesk_URL = "https://www.teamdesk.net/secure/api/v2/66383/C48C1FFD63B045BE83E44D5C1062B7AB/Trainee%20Consultants"
